fece426f3042f4648afff6f6ebd503a41872fc04
Jaggy Gauran <jaggy@artisan.studio>
2019-09-25 10:47:13 +0800

    Add the service provider methods.

--- src/AviatorServiceProvider.php
+++ src/AviatorServiceProvider.php
@@ -6,4 +6,22 @@ use Illuminate\Support\ServiceProvider;
 
 class AviatorServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
 }
